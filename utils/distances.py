1223224c80ab9aa0a84cf09537d44e159588da38
berkayturhan <r.berkayturhan@gmail.com>
2025-12-28 20:15:23 +0300

    refactor(utils): use minkowski distance for manhattan and euclidean

--- utils/distances.py
+++ utils/distances.py
@@ -1,8 +1,13 @@
+import math
+
+def minkowski(a, b, p):
+    # Formülün aslı: Mutlak değerlerin p. kuvvetlerinin toplamının (1/p). kuvveti
+    return (abs(b[0] - a[0])**p + abs(b[1] - a[1])**p) ** (1/p)
+
 def manhattan(a, b):
-    return abs(b[0] - a[0]) + abs(b[1] - a[1])
-
-
-# [...]
+    # Manhattan, Minkowski'nin p=1 halidir
+    return minkowski(a, b, 1)
 
 def euclidean(a, b):
-    return ((b[0] - a[0]) ** 2 + (b[1] - a[1]) ** 2) ** 0.5
+    # Euclidean, Minkowski'nin p=2 halidir
+    return minkowski(a, b, 2)
